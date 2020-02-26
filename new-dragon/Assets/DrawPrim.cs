9b8724dc894fc9905254b48f21331b9f071734c6
Inno <williamchendev@gmail.com>
2020-02-25 19:10:28 -0500

    Init
    Upload the project

---
+++ new-dragon/Assets/DrawPrim.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DrawPrim
+{
+    // Components
+    
+
+    // Settings
+    private int texture_width;
+
+    // Variables
+    private Color[] colors;
+
+    // Create Texture Page
+    public virtual GameObject createTextureObj() {
+        return new GameObject();
+    }
+
+    // Draw Pixel
+    public virtual void drawPixel(int x, int y, Color color) {
+        colors[x + (y * texture_width)] = color;
+    }
+
+}
+
+public class DrawPrimPartition : DrawPrim {
+
+    // Components
+
+    // Settings
+
+    // Variables
+
+    // Create Texture Page
+    public override GameObject createTextureObj() {
+        return new GameObject();
+    }
+
+    // Draw Pixel
+    public override void drawPixel(int x, int y, Color color) {
+        return;
+    }
+
+}
